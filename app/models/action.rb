3e659ac4f5fd614d19e86ceb72341b847e029c3c
guimello <guilhermesilvamello@gmail.com>
2010-04-30 03:43:31 +0000

    adding select on action layout for action_type

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,4 +2,14 @@ class Action < ActiveRecord::Base
   belongs_to  :device
   #belongs_to  :voice_command
   has_many    :schedules
+
+	validates_presence_of :command, :action_type
+	#validates_presence_of :name, :if => Proc.new {|action| action.default_name.blank?}
+
+	def self.ACTION_TYPES
+		{
+			I18n.t(:turn_on_off, :scope => [:action, :action_type, :types]) => "turn_on_off",
+			I18n.t(:range, :scope => [:action, :action_type, :types]) => "range"
+		}
+	end
 end
--- db/migrate/20100303181442_create_actions.rb
+++ db/migrate/20100303181442_create_actions.rb
@@ -5,8 +5,8 @@ class CreateActions < ActiveRecord::Migration
       t.references :voice_command,    :null => true, :foreign_key => {:dependent => :nullify}
       t.string :command,        :null => false #commando a ser enviado para o controlador
       t.string :action_type,        :null => false #tipo do comando (setar valor, faixa de valores)
-      t.integer :range_down,  :null => true #menor valor possível se possuir faixa de valores
-      t.integer :range_up,    :null => true #maior valor possível se possuir faixa de valores
+      t.integer :range_min,  :null => true #menor valor possível se possuir faixa de valores
+      t.integer :range_max,    :null => true #maior valor possível se possuir faixa de valores
       t.string  :name, :null => true #to change default name (turn on, apply, start)
 			t.timestamps
     end
