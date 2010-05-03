342453529fcd2900ded4f2d6bf2badbfc7354f54
guimello <guilhermesilvamello@gmail.com>
2010-05-03 18:22:03 +0000

    added fan and tv known devices

--- lib/known_devices.rb
+++ lib/known_devices.rb
@@ -1,8 +1,27 @@
 module KnownDevices
-	KNOWN_DEVICES = {:lamp_01 =>
-																		{	:identification => "lamp_01",
-																			:query_state => "state_lamp_01", :name => I18n.t(:name, :scope => [:device, :known, :lamp_01]),
-																			:display_icon => "lights"}}
+	KNOWN_DEVICES = {
+																	:lamp_01 =>
+																		{
+																			:identification => "lamp_01",
+																			:query_state => "state_lamp_01",
+																			:name => I18n.t(:name, :scope => [:device, :known, :lamp_01]),
+																			:display_icon => "lights"
+																		},
+																	:fan_01 =>
+																		{
+																			:identification => "fan_01",
+																			:query_state => "state_fan_01",
+																			:name => I18n.t(:name, :scope => [:device, :known, :fan_01]),
+																			:display_icon => "fan"
+																		},
+																	:tv_01 =>
+																		{
+																			:identification => "tv_01",
+																			:query_state => "state_tv_01",
+																			:name => I18n.t(:name, :scope => [:device, :known, :tv_01]),
+																			:display_icon => "television"
+																		}
+																			}
 
 	def know_this?
 		!identification.blank? and KNOWN_DEVICES.key? identification.to_sym
