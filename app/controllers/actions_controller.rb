cfcc3ac2b968d10cb4f5a218571a506f7d063b34
guimello <guilhermesilvamello@gmail.com>
2010-06-03 07:24:38 +0000

    working on indentation...

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,28 +1,28 @@
 class ActionsController < ApplicationController
-	before_filter :get_house, :get_device
+  before_filter :get_house, :get_device
 
-	def get_device
-		@device = @house.devices.find params[:device_id]
-	end
+  def get_device
+    @device = @house.devices.find params[:device_id]
+  end
 
-	def new
-		@device.actions.each do |a|
-			puts a.action_type
-		end
-	end
+  def new
+    @device.actions.each do |a|
+      puts a.action_type
+    end
+  end
 
-	def create
-		if @device.update_attributes params[:device]
-			flash[:success] = I18n.t :update, :scope => [:action, :messages, :success]
-			redirect_to new_user_house_device_action_url(@user, @house, @device)
-		else
-			render :action => :new
-		end
-	end
+  def create
+    if @device.update_attributes params[:device]
+      flash[:success] = I18n.t :update, :scope => [:action, :messages, :success]
+      redirect_to new_user_house_device_action_url(@user, @house, @device)
+    else
+      render :action => :new
+    end
+  end
 
-	def find
-		respond_to do |format|
-			format.js
-		end
-	end
+  def find
+    respond_to do |format|
+      format.js
+    end
+  end
 end
