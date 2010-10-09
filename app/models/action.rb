adb32f97c850aabab2f220984e88008860443081
guimello <guilhermesilvamello@gmail.com>
2010-10-09 14:39:10 -0300

    More comments.

--- app/models/action.rb
+++ app/models/action.rb
@@ -10,27 +10,27 @@ class Action < ActiveRecord::Base
 
   ################################################################################
   belongs_to  :device
-  has_one     :house, :through => :device
-  has_many    :logs, :conditions => {:loggable_type => 'Action'}, :order => 'created_at DESC'
+  has_one     :house, :through    => :device
+  has_many    :logs,  :conditions => {:loggable_type => 'Action'}, :order => 'created_at DESC'
   
   ################################################################################
-  validates_presence_of :command, :action_type, :query_state
-  validates_uniqueness_of :command, :scope => [:device_id]
-  validates_numericality_of :command, :only_integer => true
+  validates_presence_of     :command,     :action_type, :query_state
+  validates_uniqueness_of   :command,     :scope => [:device_id]
+  validates_numericality_of :command,     :only_integer => true
   validates_numericality_of :query_state, :only_integer => true
-  validates_inclusion_of :action_type, :in => [ActionTypes::TURN_ON_OFF, ActionTypes::RANGE]
-  validates_presence_of :name, :unless => Proc.new {|action| action.know?}
-  validates_presence_of :range_min, :range_max, :if => Proc.new {|action| action.validates_range?}
-  validates_numericality_of :range_min, :range_max, :only_integer => true, :if => Proc.new {|action| action.validates_range?}
-  validate :range_min_less_then_range_max, :if => Proc.new {|action| action.validates_range?}
+  validates_inclusion_of    :action_type, :in => [ActionTypes::TURN_ON_OFF, ActionTypes::RANGE]
+  validates_presence_of     :name,        :unless => Proc.new {|action| action.know?}
+  validates_presence_of     :range_min,   :range_max, :if => Proc.new {|action| action.validates_range?}
+  validates_numericality_of :range_min,   :range_max, :only_integer => true, :if => Proc.new {|action| action.validates_range?}
+  validate                  :range_min_less_then_range_max, :if => Proc.new {|action| action.validates_range?}
 
   ################################################################################
-  before_save :reset_ranges, :unless => Proc.new {|action| action.validates_range?}
-  before_save :reset_name, :if => Proc.new {|action| action.name == action.default_name or action.name.blank?}
+  before_save :reset_ranges,  :unless => Proc.new {|action| action.validates_range?}
+  before_save :reset_name,    :if => Proc.new {|action| action.name == action.default_name or action.name.blank?}
 
   ################################################################################
   named_scope :turn_on_off, :conditions => {:action_type => ActionTypes::TURN_ON_OFF}
-  named_scope :range, :conditions => {:action_type => ActionTypes::RANGE}
+  named_scope :range,       :conditions => {:action_type => ActionTypes::RANGE}
 
   ################################################################################
   attr_writer :temp_id
