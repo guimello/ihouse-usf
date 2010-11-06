4b03e1324e7b170bd9b74b0863af619b0b79b41c
guimello <guilhermesilvamello@gmail.com>
2010-11-06 18:22:22 -0200

    Added code examples (not useful at all).

---
+++ lib/ruby_examples.rb
@@ -0,0 +1,38 @@
+##########################################################################
+#
+#
+#3.times { puts "Em Ruby tudo é objeto!" }
+#
+#class Fixnum
+#
+#  def mais(numero)
+#    self.+ numero
+#  end
+#
+#end
+#
+#puts 5.mais 6
+#
+#
+#vetor_de_strings = %w{Java C Ruby PHP}.map do |linguagem|
+#
+#  linguagem == 'Ruby' ?
+#   'Blocos em Ruby são importantes.' :
+#   linguagem
+#
+#end
+#
+#puts vetor_de_strings.inspect
+#mensagem = 'sd'
+#
+#SerialPort.new  '/dev/tty.usbserial',
+#                9600,
+#                8,
+#                1,
+#                SerialPort::NONE do |serial|
+#  serial.puts 'iHouse USF'
+#  mensagem = serial.gets
+#end
+#
+#
+#mensagem
